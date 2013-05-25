a5c1ba08ed59d3685a32a9d8a4f33680a0e7d070
DingHui <dhjy123456@163.com>
2013-05-25 15:58:28 +0800

    测试自增自减操作符
    自增或自减操作符的后缀形式是除那些涉及赋值的操作符以外，唯一具有“副作用”的操作符

---
+++ AutoInc.java
@@ -0,0 +1,23 @@
+//: operators/AutoInc.java
+// Demonstrates the ++ and -- operators
+
+public class AutoInc{
+	public static void main(String[] args){
+		int i = 1;
+		System.out.println("i : " + i);
+		System.out.println("++i : " + ++i);
+		System.out.println("i++ : " + i++);
+		System.out.println("i : " + i);
+		System.out.println("--i : " + --i);
+		System.out.println("i-- : " + i--);
+		System.out.println("i : " + i);
+	}
+} /* Output:
+i : 1
+++i : 2
+i++ : 2
+i : 3
+--i : 2
+i-- : 2
+i : 1
+*///:~
