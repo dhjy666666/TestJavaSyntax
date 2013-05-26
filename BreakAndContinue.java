2dc91e32f1f75f2131787ded59f3af2ee979bb20
DingHui <dhjy123456@163.com>
2013-05-26 21:10:40 +0800

    测试continue和break，以及其带标签时的使用方式
    带标签的continue和break与C++有很大不同

---
+++ BreakAndContinue.java
@@ -0,0 +1,22 @@
+//: control/BreakAndContinue.java
+// Demonstrates break and continue keywords.
+
+public class BreakAndContinue{
+	public static void main(String[] args){
+		for(int i = 0; i < 100; i++){
+			if(i == 71) break; // Out of for loop
+			if(i % 9 != 0) continue; // Next iteration
+			System.out.print(i + " ");
+		}
+		System.out.println();
+		int i = 0;
+		// An "infinite loop":
+		while(true){
+			i++;
+			int j = i * 27;
+			if(j == 1269) break; // Out of loop
+			if(i % 10 != 0) continue; // Top of loop
+			System.out.print(i + " ");
+		}
+	}
+}
