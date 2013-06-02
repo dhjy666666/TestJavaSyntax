8076257979baf0ce3259f81a93293411f970a695
DingHui <dhjy123456@163.com>
2013-06-02 17:55:06 +0800

    测试Java SE5的新特性-可变参数列表

--- VarArgs.java
+++ VarArgs.java
@@ -3,7 +3,7 @@
 class A{}
 
 public class VarArgs{
-	static void printArray(Object[] args){
+	static void printArray(Object... args){
 		for(Object obj : args)
 		{
 			System.out.println(obj + " ");
@@ -12,14 +12,21 @@ public class VarArgs{
 	}
 	
 	public static void main(String[] args){
+		// Can take individual elements:
 		printArray(new Object[]{
 			new Integer(47), new Float(3.14), new Double(11.11)
 		});
-		printArray(new Object[]{"one", "two", "three" });
-		printArray(new Object[]{new A(), new A(), new A()});
+		printArray(47, 3.14F, 11.11);
+		printArray("one", "two", "three");
+		printArray(new A(), new A(), new A());
+		// Or an array:
+		printArray((Object[]) new Integer[]{1, 2, 3, 4});
+		printArray(); // Empty list is OK
 	}
 } /* Output: (sample)
 47 3.14 11.11
+47 3.14 11.11
 one two three
 A@1a46e30 A@3e25a5 A@19821f
+1 2 3 4
 *///:~
