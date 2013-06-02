2c15e816e05061d2436eb723896bf6fb776d98c9
DingHui <dhjy123456@163.com>
2013-06-02 23:00:17 +0800

    测试import static语句

---
+++ PrintTest.java
@@ -0,0 +1,16 @@
+//: PrintTest.java
+import static TestJavaSyntax.util.Print.*;
+
+public class PrintTest{
+	public static void main(String[] args){
+		print("Available from now on!");
+		print(100);
+		print(100L);
+		print(3.14159);
+	}
+} /* Output:
+Available from now on!
+100
+100
+3.14159
+*///:~
---
+++ util/Print.java
@@ -0,0 +1,24 @@
+//: util/Print.java
+// Print methods that can be used without
+// qualifiers, using Java SE5 static imports:
+package TestJavaSyntax.util;
+import java.io.*;
+
+public class Print{
+	// Print with a newline:
+	public static void print(Object obj){
+		System.out.println(obj);
+	}
+	// Print a newline by itself:
+	public static void print(){
+		System.out.println();
+	}
+	// Print with no line break:
+	public static void printnb(Object obj){
+		System.out.println(obj);
+	}
+	// The new Java SE5 printf() (from c):
+	public static PrintStream printf(String format, Object... args){
+		return System.out.printf(format, args);
+	}
+} ////:~
