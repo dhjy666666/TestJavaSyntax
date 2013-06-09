e09518e05f1f87ce028affe38fa364404e1749cb
DingHui <dhjy123456@163.com>
2013-06-09 15:12:16 +0800

    测试多态和向上转型
    注意含包的程序的运行方式

---
+++ polymorphism/music/Instrument.java
@@ -0,0 +1,10 @@
+//: polymorphism/music/Instrument.java
+// Notes to play on musical instruments.
+package TestJavaSyntax.polymorphism.music;
+import static TestJavaSyntax.util.Print.*;
+
+class Instrument{
+	public void play(Note n){
+		print("Instrument.play()");
+	}
+} ///:~
